81d87c7c6dada83f5d4d2988a115698954b10eda
Jon <sanchezeguiajon@gmail.com>
2024-04-11 01:01:53 +0200

    home bug fix

--- app/views/home.php
+++ app/views/home.php
@@ -35,13 +35,15 @@ if ($sessiont < 3) {
             <div class="card">
                 <div class="card-body flex justify-between">
                     <h2 class="card-title"><?php echo $mensaje; ?></h2>
-                    <?php if ( $sessiont == 2): ?>
-                        <a href="adminpanel" class="text-blue-500">
-                            <button type="button" class="px-4 py-2 rounded-md bg-blue-200 hover:bg-blue-300 focus:outline-none focus:bg-blue-300">Ir al panel de administración</button>
-                        </a>
+                    <?php if ( $sessiont < 3): ?>
 						<form action="logout" method="post" onsubmit="return confirm('¿Estás seguro de que quieres cerrar tu sesión?');">
                         	<button type="submit" class="btn-danger px-4 py-2 rounded-md bg-red-500 hover:bg-red-600 focus:outline-none focus:bg-red-600">Cerrar sesión</button>
                     	</form>
+						<?php if ($sessiont == 2): ?>
+							<a href="adminpanel" class="text-blue-500">
+								<button type="button" class="px-4 py-2 rounded-md bg-blue-200 hover:bg-blue-300 focus:outline-none focus:bg-blue-300">Ir al panel de administración</button>
+							</a>
+						<?php endif; ?>
 					<?php else: ?>
 						<a href="login" class="text-blue-500">
                             <button type="button" class="px-4 py-2 rounded-md bg-blue-200 hover:bg-blue-300 focus:outline-none focus:bg-blue-300">Iniciar sesión</button>
