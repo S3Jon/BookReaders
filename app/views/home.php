ba6e5a06632587fb02bf0dec78dc7042f5265129
Jon <sanchezeguiajon@gmail.com>
2024-04-10 17:23:02 +0200

    Update home.php (nosession needed)

--- app/views/home.php
+++ app/views/home.php
@@ -2,12 +2,22 @@
 
 session_start();
 
-if (!isset($_SESSION['userData']) || empty($_SESSION['userData']['username'])) {
-    header('Location: login');
-    exit;
+//TODO - esto es fix temporal, mejorar sistema
+//mensaje inicio
+
+
+if (isset($_SESSION['userData']) && !empty($_SESSION['userData']['username'])) {
+	$sessiont = $_SESSION['userData']['role'] == 'admin' ? 2 : 1;
 } else {
-    $userData = $_SESSION['userData'];
+	$sessiont = 3;	
 }
+
+if ($sessiont < 3) {
+	$mensaje = "¡Bienvenido, " . $_SESSION['userData']['username'] . "!";
+} else {
+	$mensaje = "¡Bienvenido!";
+}
+
 
 ?>
 
@@ -24,15 +34,19 @@ if (!isset($_SESSION['userData']) || empty($_SESSION['userData']['username'])) {
         <div class="md:w-1/2 md:mx-auto">
             <div class="card">
                 <div class="card-body flex justify-between">
-                    <h2 class="card-title">¡Bienvenido, <?php echo htmlspecialchars($userData['username']); ?>!</h2>
-                    <?php if ($userData['role'] === 'admin'): ?>
+                    <h2 class="card-title"><?php echo $mensaje; ?></h2>
+                    <?php if ( $sessiont == 2): ?>
                         <a href="adminpanel" class="text-blue-500">
                             <button type="button" class="px-4 py-2 rounded-md bg-blue-200 hover:bg-blue-300 focus:outline-none focus:bg-blue-300">Ir al panel de administración</button>
                         </a>
+						<form action="logout" method="post" onsubmit="return confirm('¿Estás seguro de que quieres cerrar tu sesión?');">
+                        	<button type="submit" class="btn-danger px-4 py-2 rounded-md bg-red-500 hover:bg-red-600 focus:outline-none focus:bg-red-600">Cerrar sesión</button>
+                    	</form>
+					<?php else: ?>
+						<a href="login" class="text-blue-500">
+                            <button type="button" class="px-4 py-2 rounded-md bg-blue-200 hover:bg-blue-300 focus:outline-none focus:bg-blue-300">Iniciar sesión</button>
+                        </a>
                     <?php endif; ?>
-                    <form action="logout" method="post" onsubmit="return confirm('¿Estás seguro de que quieres cerrar tu sesión?');">
-                        <button type="submit" class="btn-danger px-4 py-2 rounded-md bg-red-500 hover:bg-red-600 focus:outline-none focus:bg-red-600">Cerrar sesión</button>
-                    </form>
                 </div>
             </div>
         </div>
