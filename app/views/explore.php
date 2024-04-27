5b4c48171ec754419bf5600fad8103eb070af2c5
RubenSans <rubensanbel@campus.monlau.com>
2024-04-27 20:39:55 +0200

    prueba

---
+++ app/views/explore.php
@@ -0,0 +1,13 @@
+<?php
+
+?>
+
+<?php require_once 'partials/header.php'; ?>
+
+<div class="min-h-screen">
+    <div class="grid text-7xl place-content-center h-[80vh]">
+        Home
+    </div>
+</div>
+
+<?php require_once 'partials/footer.php'; ?>
