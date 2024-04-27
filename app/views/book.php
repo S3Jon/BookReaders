6f7ff03bebb68ae0c6cb319321a093e1077f41ff
RubenSans <rubensanbel@campus.monlau.com>
2024-04-27 14:16:57 +0200

    book ready

--- app/views/book.php
+++ app/views/book.php
@@ -7,19 +7,17 @@
 <?php include_once 'partials/header.php'; ?>
 
 <div class="container mx-auto mt-5 h-screen">
-    <div class="md:w-1/2 md:mx-auto">
-        <div class="flex justify-between items-center">
-            <h1 class="text-2xl font-bold">Book</h1>
-            <a href="/BookReaders/" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Back</a>
+    <div class="md:w-[60%] md:mx-auto">
+        <div class="flex ">
+           <aside class="flex flex-col items-center gap-5 w-52">
+                <img src="uploads/kafka.svg" alt="kafka">
+                <button class="w-3/4 p-2 bg-accent font-semibold rounded-md">Añadir a favoritos</button>
+                <button class="w-3/4 p-2 bg-primary text-background font-semibold rounded-md">Añadir a una lista</button>
+           </aside>
+           <article>
+                
+           </article>
         </div>
-        <div class="mt-5">
-            <p class="text-lg">Title: <span class="font-bold">Book Title</span></p>
-            <p class="text-lg">Author: <span class="font-bold">Author Name</span></p>
-            <p class="text-lg">Year: <span class="font-bold">Year</span></p>
-            <p class="text-lg">Genre: <span class="font-bold">Genre</span></p>
-            <p class="text-lg">Description: <span class="font-bold">Description</span></p>
-        </div>
-
     </div>
 </div>
 
