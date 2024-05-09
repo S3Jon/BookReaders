db3b67bb50889b72e6b105df0b4dc5d440e980d2
Jon <sanchezeguiajon@gmail.com>
2024-05-09 07:57:51 +0200

    follow count on list view

--- app/views/list_derivadas/list_info.php
+++ app/views/list_derivadas/list_info.php
@@ -1,6 +1,7 @@
 <h1 class="text-lg font-semibold text-gray-900"><?= $nombreLista ?></h1>
 	<p class="text-sm text-gray-600">Propietario: <?= $propietarioLista ?></p>
 	<p class="text-sm text-gray-600">Visibilidad: <?= $visibilidadLista ?></p>
+	<p class="text-sm text-gray-600">Seguidores: <?= $UFLController->getFollowers($IDforJS) ?></p>
 	<?php if ($listOS): ?>
 		<form action="edit_list" method="POST">
 			<input type="hidden" name="id_list" value="<?= $_POST['id_list'] ?>">
