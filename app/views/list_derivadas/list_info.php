11a0629f570fe73df177910f501a94bd8b917c9e
Jon <sanchezeguiajon@gmail.com>
2024-05-27 00:07:21 +0200

    basic list not deleteable

--- app/views/list_derivadas/list_info.php
+++ app/views/list_derivadas/list_info.php
@@ -15,10 +15,12 @@
 	<?php if ($listOS): ?>
 		<p class="text-sm text-gray-600">Visibilidad: <?= $visibilidadLista ?></p>
 		<a href="list_edit?id=<?= $id_list ?>" class="px-2 py-1 bg-green-500 text-white rounded-md hover:bg-green-600 focus:outline-none focus:bg-green-600">Editar lista</a>
-		<form action="list" method="POST">
-			<input type="hidden" name="id_list" value="<?= $id_list ?>">
-			<button type="submit" name="delete_list" onclick="return confirm('¿Seguro que quieres eliminar esta lista?');" class="px-2 py-1 bg-red-500 text-white rounded-md hover:bg-red-600 focus:outline-none focus:bg-red-600">Eliminar lista</button>
-		</form>
+		<?php if (!$listaInfo['type']) : ?>
+			<form action="list" method="POST">
+				<input type="hidden" name="id_list" value="<?= $id_list ?>">
+				<button type="submit" name="delete_list" onclick="return confirm('¿Seguro que quieres eliminar esta lista?');" class="px-2 py-1 bg-red-500 text-white rounded-md hover:bg-red-600 focus:outline-none focus:bg-red-600">Eliminar lista</button>
+			</form>
+		<?php endif; ?>
 	<?php else: ?>
 		<?php if (!isset($_SESSION['userData'])): ?>
 			<button class="px-2 py-1 bg-green-500 text-white rounded-md hover:bg-green-600 focus:outline-none focus:bg-green-600" onclick="window.location.href = 'login';">Seguir lista</button>
